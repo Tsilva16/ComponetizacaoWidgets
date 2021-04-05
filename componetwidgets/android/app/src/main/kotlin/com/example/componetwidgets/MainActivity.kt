075738f4274d326b0fafddb639ef89aef39aac1d
Tsilva16 <tisolesilva@gmail.com>
2021-04-05 19:51:45 -0300

    InicioComponetização

---
+++ componetwidgets/android/app/src/main/kotlin/com/example/componetwidgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.componetwidgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ componetwidgets/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Column(
+        children: [
+          Container(
+            height: 200,
+            width: 200,
+            color: Colors.blue,
+          ),
+          Container(
+            height: 200,
+            width: 200,
+            color: Colors.blue,
+          ),
+          Container(
+            height: 200,
+            width: 200,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+    );
+  }
+}
